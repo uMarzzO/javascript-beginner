19c2100d418935c792ac1c8eff46ee8911ab5591
Bruno Marzano <umarzzo92@gmail.com>
2022-03-26 05:02:16 -0300

    up news

--- exercicios/aula014exp4/ex016/script.js
+++ exercicios/aula014exp4/ex016/script.js
@@ -1,1 +1,24 @@
-alert(`Olá, Mundo!!`)
+function contar() {
+    let ini = document.getElementById(`txti`)
+    let fim = document.getElementById(`txtf`)
+    let passo = document.getElementById(`txtp`)
+    let res = document.getElementById(`res`)
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert(`[ERRO] Faltam dados!`)
+    } else {
+        res.innerHTML = `Contando: `
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (i < f) {
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+        }
+        res.innerHTML += `\u{1F3C1}`
+        } else {
+            
+        }
+    }
+}
