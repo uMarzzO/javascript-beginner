75c02f4028615ded21748e8578099e7e7612f90e
Bruno Marzano <umarzzo92@gmail.com>
2022-03-26 15:12:16 -0300

    update script

--- exercicios/aula014exp4/ex017/script.js
+++ exercicios/aula014exp4/ex017/script.js
@@ -1,1 +1,18 @@
-alert(`Olá, Mundo!!`)
+function tabuada() {
+    let num = document.getElementById(`txtn`)
+    let tab = document.getElementById(`seltab`)
+if (num.value.length == 0) {
+        window.alert(`Por favor, digite um número!`)
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ``
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement(`option`)
+                        
+            item.text = `${n} * ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+            
+    }
