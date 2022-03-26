ac9b837732c92504f3337507d9a27b47b31b96b2
Bruno Marzano <umarzzo92@gmail.com>
2022-03-26 05:15:11 -0300

    update scripts

--- exercicios/aula014exp4/ex016/script.js
+++ exercicios/aula014exp4/ex016/script.js
@@ -5,20 +5,32 @@ function contar() {
     let res = document.getElementById(`res`)
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert(`[ERRO] Faltam dados!`)
+        res.innerHTML = `Impossível contar!`
+        //window.alert(`[ERRO] Faltam dados!`)
+
     } else {
-        res.innerHTML = `Contando: `
+        res.innerHTML = `Contando: <br>`
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert(`Passo inválido! Considerando PASSO 1.`)
+            p = 1
+        }
 
         if (i < f) {
-            for (var c = i; c <= f; c += p) {
+            //Contagem crescente
+            for (let c = i; c <= f; c += p) {
                 res.innerHTML += `${c} \u{1F449}`
+            }
+
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+
         }
         res.innerHTML += `\u{1F3C1}`
-        } else {
-            
-        }
     }
 }
